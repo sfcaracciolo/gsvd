7d615e7ec990ec2750a3166d970dea9b6b6bc093
scaracciolo <santiago.caracciolo@gmail.com>
2023-10-04 17:55:09 -0300

    testing ..

--- src/gsvd/core.py
+++ src/gsvd/core.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 import numpy as np 
 import scipy as sp 
 
@@ -11,40 +12,59 @@ def gsvd(A: np.ndarray, B:np.ndarray, tol: float = 1e-8, return_extras: bool = F
     
 
     p, n = A.shape
-    m1, _ = B.shape
+    m1, n = B.shape
 
     M = np.vstack([A, B])
-    m, _ = M.shape
+    m, n = M.shape
+
     Q, sv, Zt = np.linalg.svd(M, full_matrices=True, compute_uv=True)
+
     sv[sv<tol] = 0
     r = np.count_nonzero(sv) # rank(M)
     m2 = m - r
 
-    if p < r:
-        raise ValueError('Rows of A must be grather than rank([A, B]).')
+    if m <= r:
+        raise ValueError('Rows of [A,B] must be grather than rank([A, B]).')
 
-    if m1 < r:
-        raise ValueError('Rows of B must be grather than rank([A, B]).')
+    # if p < r:
+    #     raise ValueError('Rows of A must be grather than rank([A, B]).')
+
+    # if m1 < r:
+    #     raise ValueError('Rows of B must be grather than rank([A, B]).')
     
-    U, CS, Vt = sp.linalg.cossin(Q, p, r, separate=False)
+    U, D, Vt = sp.linalg.cossin(Q, p=p, q=r, separate=False)
 
     U_1 = U[:p,:p] # pxp
     U_2 = U[p:,p:] # m1xm1
     V_1t = Vt[:r,:r] # rxr
     V_2t = Vt[r:,r:] # m2xm2
 
-    D_11 = sp.sparse.hstack((CS[:p,:r], sp.sparse.csc_array((p, n-r)))) # pxr + px(n-r)
-    D_21 = sp.sparse.hstack((CS[p:,:r], sp.sparse.csc_array((m1, n-r)))) # m1xr + m1x(n-r)
+    D_11, D_12 = D[:p,:r], D[:p,r:]
+    D_21, D_22 = D[p:,:r], D[p:,r:]
 
     I = sp.sparse.identity(n-r, format='dia', dtype=np.float64)
     Sr = sp.sparse.dia_matrix((sv[:r], 0), shape=(r,r), dtype=np.float64)
     W = sp.sparse.block_diag((V_1t @ Sr, I)).tocsc()
     X = Zt.T @ sp.sparse.linalg.inv(W)
     
-    if return_extras:
-        C = CS[:r,:r]
-        S = CS[-r:,:r]
-        extras = dict( cs=(C, S), rank=r)
-        return (U_1, U_2), (D_11, D_21), X, extras
-    
-    return (U_1, U_2), (D_11, D_21), X
+    w = min([p, r, m1, m2])
+    fmin = lambda a, b: min([a, b])-w
+    fmax = lambda a, b: max([a-b, 0])
+
+    a = fmin(p,r)
+    C = D_11[a:a+w, a:a+w]
+
+    b = fmax(m1,r)
+    c = fmax(r,m1)
+    print(b, c)
+    S = D_21[b:b+w, c:c+w]
+
+    print(np.allclose(Q, U@D@Vt))
+    plt.spy(D)
+    plt.spy(D_21)
+    plt.show()
+    D_A = sp.sparse.hstack((D_11, sp.sparse.csc_array(( p, n-r)))) # pxr + px(n-r)
+    D_B = sp.sparse.hstack((D_21, sp.sparse.csc_array((m1, n-r)))) # m1xr + m1x(n-r)
+
+    if return_extras: return (U_1, U_2), (D_A, D_B), X, dict(cs=(C, S), rank=r)
+    return (U_1, U_2), (D_A, D_B), X
--- tests/gsvd.py
+++ tests/gsvd.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 from src.gsvd import gsvd
 import numpy as np 
 
@@ -5,29 +6,28 @@ rng = np.random.default_rng()
 
 for _ in range(100):
 
-    p = rng.integers(50, high=100)
-    n = rng.integers(25, high=50)
-    m1 = rng.integers(50, high=150)
+    p = rng.integers(25, high=100)
+    n = rng.integers(25, high=100)
+    m = rng.integers(p, high=p+50)
 
     A = np.random.rand(p, n)
-    B = np.random.rand(m1, n)
+    B = np.random.rand(m-p, n)
 
     ker = rng.integers(0,n//3, 1)[0]
 
-    print(p, m1, n, ker)
+    print(p, m, n, ker)
 
-    A[:,:ker+1] = 1
-    B[:,:ker+1] = 1
+    # A[:,:ker+1] = 1
+    # B[:,:ker+1] = 1
 
     (U_1, U_2), (D_A, D_B), X, extras = gsvd(A, B, return_extras=True)
 
     C, S = extras['cs']
-    r = extras['rank']
-    
-    assert np.allclose((C.T @ C + S.T @ S), np.eye(r), rtol=1e-5, atol=1e-8)
+    w = C.shape[0]
+
+    # plt.spy(D_B)
+    # plt.show()
+    assert np.allclose(C**2 + S**2, np.eye(w), rtol=1e-5, atol=1e-8)
     assert np.allclose(D_A.todense(), U_1.T @ A @ X, rtol=1e-5, atol=1e-8)
     assert np.allclose(D_B.todense(), U_2.T @ B @ X, rtol=1e-5, atol=1e-8)
-    assert np.allclose(U_1 @ D_A @ np.linalg.inv(X), A, rtol=1e-5, atol=1e-8)
-    assert np.allclose(U_2 @ D_B @ np.linalg.inv(X), B, rtol=1e-5, atol=1e-8)
-
 print('OK')
