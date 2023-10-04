015e2fbc85015e45c09de0d7765702e44945110c
scaracciolo <santiago.caracciolo@gmail.com>
2023-10-04 18:54:19 -0300

    checkpoint

--- src/gsvd/core.py
+++ src/gsvd/core.py
@@ -10,6 +10,8 @@ def gsvd(A: np.ndarray, B:np.ndarray, tol: float = 1e-8, return_extras: bool = F
     if A.shape[1] != B.shape[1]:
         raise ValueError('Last dimension of A & B must be equal.')
     
+    if (0 in A.shape) or (0 in B.shape):
+        raise ValueError('A and/or B has a dimension of size zero.')
 
     p, n = A.shape
     m1, n = B.shape
@@ -26,12 +28,6 @@ def gsvd(A: np.ndarray, B:np.ndarray, tol: float = 1e-8, return_extras: bool = F
     if m <= r:
         raise ValueError('Rows of [A,B] must be grather than rank([A, B]).')
 
-    # if p < r:
-    #     raise ValueError('Rows of A must be grather than rank([A, B]).')
-
-    # if m1 < r:
-    #     raise ValueError('Rows of B must be grather than rank([A, B]).')
-    
     U, D, Vt = sp.linalg.cossin(Q, p=p, q=r, separate=False)
 
     U_1 = U[:p,:p] # pxp
@@ -42,27 +38,32 @@ def gsvd(A: np.ndarray, B:np.ndarray, tol: float = 1e-8, return_extras: bool = F
     D_11, D_12 = D[:p,:r], D[:p,r:]
     D_21, D_22 = D[p:,:r], D[p:,r:]
 
-    I = sp.sparse.identity(n-r, format='dia', dtype=np.float64)
-    Sr = sp.sparse.dia_matrix((sv[:r], 0), shape=(r,r), dtype=np.float64)
-    W = sp.sparse.block_diag((V_1t @ Sr, I)).tocsc()
-    X = Zt.T @ sp.sparse.linalg.inv(W)
-    
     w = min([p, r, m1, m2])
     fmin = lambda a, b: min([a, b])-w
     fmax = lambda a, b: max([a-b, 0])
 
+    b = fmax(m1,r)
+    c = fmax(r,m1)
+    S = D_21[b:b+w, c:c+w]
+
+    D_21 = sp.sparse.block_diag((
+        sp.sparse.csc_array(( fmax(m1, r), fmax(r, m1))),
+        S,
+        sp.sparse.identity(fmin(m1,r), format='dia', dtype=np.float64)
+    ))
+
+    I = sp.sparse.identity(n-r, format='dia', dtype=np.float64)
+    Sr = sp.sparse.dia_matrix((sv[:r], 0), shape=(r,r), dtype=np.float64)
+    W = sp.sparse.block_diag((V_1t @ Sr, I)).tocsc()
+    X = Zt.T @ sp.sparse.linalg.inv(W)
+
     a = fmin(p,r)
     C = D_11[a:a+w, a:a+w]
 
-    b = fmax(m1,r)
-    c = fmax(r,m1)
-    print(b, c)
-    S = D_21[b:b+w, c:c+w]
-
-    print(np.allclose(Q, U@D@Vt))
-    plt.spy(D)
-    plt.spy(D_21)
-    plt.show()
+    # plt.spy(D)
+    # plt.axhline(p)
+    # plt.axvline(r)
+    # plt.show()
     D_A = sp.sparse.hstack((D_11, sp.sparse.csc_array(( p, n-r)))) # pxr + px(n-r)
     D_B = sp.sparse.hstack((D_21, sp.sparse.csc_array((m1, n-r)))) # m1xr + m1x(n-r)
 
--- tests/gsvd.py
+++ tests/gsvd.py
@@ -4,21 +4,23 @@ import numpy as np
 
 rng = np.random.default_rng()
 
-for _ in range(100):
+for _ in range(1000):
 
     p = rng.integers(25, high=100)
     n = rng.integers(25, high=100)
-    m = rng.integers(p, high=p+50)
+    m = rng.integers(p+1, high=p+50)
 
     A = np.random.rand(p, n)
     B = np.random.rand(m-p, n)
 
     ker = rng.integers(0,n//3, 1)[0]
+    A[:,:ker+1] = 1
+    B[:,:ker+1] = 1
 
-    print(p, m, n, ker)
-
-    # A[:,:ker+1] = 1
-    # B[:,:ker+1] = 1
+    r = np.linalg.matrix_rank(np.vstack([A,B]), tol=1e-8)
+    print(f'p={p}, r={r}, m1={m-p}, m2={m-r}, n={n}')
+    if m <= r:
+        continue
 
     (U_1, U_2), (D_A, D_B), X, extras = gsvd(A, B, return_extras=True)
 
