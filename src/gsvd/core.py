fd18845e003cff8d9e7a27e77820c0371907cb62
scaracciolo <santiago.caracciolo@gmail.com>
2023-10-09 17:57:13 -0300

    cs decomposition delegated to /cossin_wrapper

--- src/gsvd/core.py
+++ src/gsvd/core.py
@@ -1,111 +1,7 @@
-from typing import Literal, Tuple
-from matplotlib import pyplot as plt
 import numpy as np 
 import scipy as sp 
+from cossin_wrapper import cossin
 
-
-def cs2(Q: np.ndarray, shape: Tuple[int, int], ret: Literal['full', 'blocks', 'cs'] = 'full'):
-    """
-    Note: I think cossin function has a issue when separate=False, because in some cases the struct of D is incorrect. 
-    The workaround is use separate=True and build each matrix manually.
-    FAIL WHEN n < m + p + 1
-    """
-
-    p, r = shape
-    m, n = Q.shape
-    m1, m2 = m - p, m - r 
-
-    U, D, Vt = sp.linalg.cossin(Q, p=p, q=r, separate=False)
-
-    assert np.allclose(U@D@Vt, Q)
-
-    w = min([p, r, m1, m2])
-    fmin = lambda a, b: min([a, b])-w
-    fmax = lambda a, b: max([a-b, 0])
-
-    D_11 = D[:p, :r]
-    D_21 = D[p:, :r]
-    D_12 = D[:p, r:]
-    D_22 = D[p:, r:]
-
-    U_1, U_2 = U[:p, :p], U[p:, p:]
-    V_1t, V_2t = Vt[:r, :r], Vt[r:, r:]
-
-    C = sp.sparse.dia_matrix((D_11[fmin(p,r):fmin(p,r)+w, fmin(p,r):fmin(p,r)+w].diagonal(), 0), shape=(w,w), dtype=np.float64) 
-    S = sp.sparse.dia_matrix((D_21[fmax(m1,r):fmax(m1,r)+w, fmax(r,m1):fmax(r,m1)+w].diagonal(), 0), shape=(w,w), dtype=np.float64) 
-
-    if ret == 'full':
-        return U, D, Vt
-
-    if ret == 'blocks':
-        D_11 = sp.sparse.dia_matrix((D_11.diagonal(), 0), shape=(p,r), dtype=np.float64)
-        D_21 = sp.sparse.dia_matrix((D_21.diagonal(), 0), shape=(m1,r), dtype=np.float64)
-        D_12 = sp.sparse.dia_matrix((D_12.diagonal(), 0), shape=(p,m2), dtype=np.float64)
-        D_22 = sp.sparse.dia_matrix((D_22.diagonal(), 0), shape=(m1,m2), dtype=np.float64)
-        return (U_1, U_2), (D_11, D_12, D_21, D_22), (V_1t, V_2t)
-
-    if ret == 'cs':
-        return C, S 
-    
-
-def cs(Q: np.ndarray, shape: Tuple[int, int], ret: Literal['full', 'blocks', 'cs'] = 'full'):
-    """
-    Note: I think cossin function has a issue when separate=False, because in some cases the struct of D is incorrect. 
-    The workaround is use separate=True and build each matrix manually.
-    """
-
-    p, r = shape
-    m, n = Q.shape
-    m1, m2 = m - p, m - r 
-
-    (U_1, U_2), theta, (V_1t, V_2t) = sp.linalg.cossin(Q, p=p, q=r, separate=True)
-
-    w = min([p, r, m1, m2])
-    fmin = lambda a, b: min([a, b])-w
-    fmax = lambda a, b: max([a-b, 0])
-
-    C = sp.sparse.dia_matrix((np.cos(theta), 0), shape=(w,w), dtype=np.float64) 
-    S = sp.sparse.dia_matrix((np.sin(theta), 0), shape=(w,w), dtype=np.float64) 
-
-    D_11 = sp.sparse.block_diag((
-        sp.sparse.identity(fmin(p,r), format='dia', dtype=np.float64),
-        C,
-        sp.sparse.csc_array((fmax(p,r), fmax(r,p)), dtype=np.float64)
-    ))
-
-    D_21 = sp.sparse.block_diag((
-        sp.sparse.csc_array((fmax(m1,r), fmax(r,m1)), dtype=np.float64),
-        S,
-        sp.sparse.identity(fmin(m1,r), format='dia', dtype=np.float64)
-    ))
-
-    D_12 = sp.sparse.block_diag((
-        sp.sparse.csc_array((fmax(p,m2), fmax(m2,p)), dtype=np.float64),
-        -S,
-        -sp.sparse.identity(fmin(m2,p), format='dia', dtype=np.float64)
-    ))
-
-    D_22 = sp.sparse.block_diag((
-        sp.sparse.identity(fmin(m1, m2), format='dia', dtype=np.float64),
-        C,
-        sp.sparse.csc_array((fmax(m1,m2), fmax(m2,m1)), dtype=np.float64)
-    ))
-
-    if ret == 'full':
-        U = sp.sparse.block_diag((U_1, U_2))
-        D = sp.sparse.vstack((
-            sp.sparse.hstack((D_11, D_12)),
-            sp.sparse.hstack((D_21, D_22)),
-        ))
-        Vt = sp.sparse.block_diag((V_1t, V_2t))
-        return U, D, Vt
-
-    if ret == 'blocks':
-        return (U_1, U_2), (D_11, D_12, D_21, D_22), (V_1t, V_2t)
-
-    if ret == 'cs':
-        return C, S 
-    
 def gsvd(A: np.ndarray, B:np.ndarray, tol: float = 1e-8):
     """
     Generalized singular value decomposition. 
@@ -132,7 +28,7 @@ def gsvd(A: np.ndarray, B:np.ndarray, tol: float = 1e-8):
     if m <= r:
         raise ValueError('Rows of [A,B] must be grather than rank([A, B]).')
 
-    (U_1, U_2), (D_11, D_12, D_21, D_22), (V_1t, V_2t) = cs(Q, shape=(p, r), ret='blocks')
+    (U_1, U_2), (D_11, D_12, D_21, D_22), (V_1t, V_2t) = cossin(Q, shape=(p, r), ret='blocks')
 
     #  (AB)^-1 = B^-1 A^-1
     Srm1 = sp.sparse.dia_matrix((1./sv[:r], 0), shape=(r,r), dtype=np.float64)
@@ -145,40 +41,3 @@ def gsvd(A: np.ndarray, B:np.ndarray, tol: float = 1e-8):
     D_B.resize((m1,n)) # m1xr + m1x(n-r) (padding zeros)
 
     return (U_1, U_2), (D_A, D_B), X
-
-# def tkh_factors(A: np.ndarray, B:np.ndarray, tol: float = 1e-8):
-#     """
-#    Tikhonov factors alpha & beta
-#     """
-
-#     if A.shape[1] != B.shape[1]:
-#         raise ValueError('Last dimension of A & B must be equal.')
-    
-#     if (0 in A.shape) or (0 in B.shape):
-#         raise ValueError('A and/or B has a dimension of size zero.')
-
-#     p, n = A.shape
-#     m1, n = B.shape
-
-#     M = np.vstack([A, B], dtype=np.float64)
-#     m, n = M.shape
-
-#     Q, sv, Zt = sp.linalg.svd(M, full_matrices=True, compute_uv=True, lapack_driver='gesvd')
-
-#     sv[sv<tol] = 0
-#     r = np.count_nonzero(sv) # rank(M)
-#     m2 = m - r
-
-#     if m <= r:
-#         raise ValueError('Rows of [A,B] must be grather than rank([A, B]).')
-
-#     C, S = cs(Q, shape=(p, r), ret='cs')
-
-#     w = min([p, r, m1, m2])
-#     fmin = lambda a, b: min([a, b])-w
-#     fmax = lambda a, b: max([a-b, 0])
-
-#     alphas = np.concatenate((np.ones(fmin(p, r)), C.diagonal(), np.zeros(fmax(r, p))))
-#     betas = np.concatenate((np.zeros(fmax(r, m1)), S.diagonal(), np.zeros(fmin(m1, r))))
-#     return alphas, betas
-    
--- tests/gsvd.py
+++ tests/gsvd.py
@@ -1,4 +1,3 @@
-from matplotlib import pyplot as plt
 from src.gsvd import gsvd
 import numpy as np 
 
@@ -9,7 +8,6 @@ for _ in range(100):
     p = rng.integers(25, high=100)
     m = rng.integers(p+1, high=p+50)
     n = rng.integers(25, high=100)
-    # n = m + p + 1  # rng.integers(25, high=100)
 
     A = np.random.rand(p, n)
     B = np.random.rand(m-p, n)
@@ -19,16 +17,15 @@ for _ in range(100):
     A[:,:ker+1] = 1
     B[:,:ker+1] = 1
 
-    r = np.linalg.matrix_rank(np.vstack([A,B]), tol=1e-8)
-    print(f'p={p}, r={r}, m1={m-p}, m2={m-r}, n={n}')
+    C = np.vstack([A,B],dtype=np.float64)
+    r = np.linalg.matrix_rank(C, tol=1e-8)
     if m <= r: # this case is not supported.
         continue
+    print(f'p={p}, r={r}, m1={m-p}, m2={m-r}, n={n}')
 
     (U_1, U_2), (D_A, D_B), X = gsvd(A, B)
 
-    # plt.spy(D_B)
-    # plt.show()
-    assert np.allclose(D_A.todense(), U_1.T @ A @ X, rtol=1e-5, atol=1e-8)
-    assert np.allclose(D_B.todense(), U_2.T @ B @ X, rtol=1e-5, atol=1e-8)
+    assert np.allclose(D_A.todense(), U_1.T @ A @ X)
+    assert np.allclose(D_B.todense(), U_2.T @ B @ X)
 
 print('OK')
